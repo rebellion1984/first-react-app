88d15988db04c00b8d2b60b1bd7884770b45d629
rebellion1984 <robel_beyene84@yahoo.co.uk>
2023-11-14 18:22:01 +0100

    WIP

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   function deleteHandler() {
-    console.log("Clicked");
-    console.log(props.text);
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
   }
 
   return (
@@ -12,6 +22,10 @@ function Todo(props) {
           Delete
         </button>
       </div>
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
